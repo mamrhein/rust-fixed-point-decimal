d0a42ad549433a1ef1c12bbc580506a34065d9b9
Michael Amrhein <michael@adrhinum.de>
2021-09-14 19:19:17 +0200

    Added impls of Add/Sub between Decimals and Integers.

--- src/binops/add_sub.rs
+++ src/binops/add_sub.rs
@@ -12,21 +12,13 @@ use std::{
     ops::{Add, Sub},
 };
 
-use num::Zero;
+use num::{Integer, Zero};
 use rust_fixed_point_decimal_core::mul_pow_ten;
 
 use crate::{
     prec_constraints::{PrecLimitCheck, True},
     Decimal,
 };
-
-pub const fn const_max_u8(a: u8, b: u8) -> u8 {
-    if a > b {
-        a
-    } else {
-        b
-    }
-}
 
 impl<const P: u8> Zero for Decimal<P>
 where
@@ -41,6 +33,33 @@ where
     #[inline(always)]
     fn is_zero(&self) -> bool {
         self.coeff.is_zero()
+    }
+}
+
+#[cfg(test)]
+mod zero_tests {
+    use super::*;
+
+    #[test]
+    fn test_zero() {
+        assert!(Decimal::<0>::is_zero(&Decimal::<0>::zero()));
+        assert!(Decimal::<1>::is_zero(&Decimal::<1>::zero()));
+        assert!(Decimal::<2>::is_zero(&Decimal::<2>::zero()));
+        assert!(Decimal::<3>::is_zero(&Decimal::<3>::zero()));
+        assert!(Decimal::<4>::is_zero(&Decimal::<4>::zero()));
+        assert!(Decimal::<5>::is_zero(&Decimal::<5>::zero()));
+        assert!(Decimal::<6>::is_zero(&Decimal::<6>::zero()));
+        assert!(Decimal::<7>::is_zero(&Decimal::<7>::zero()));
+        assert!(Decimal::<8>::is_zero(&Decimal::<8>::zero()));
+        assert!(Decimal::<9>::is_zero(&Decimal::<9>::zero()));
+    }
+}
+
+pub const fn const_max_u8(a: u8, b: u8) -> u8 {
+    if a > b {
+        a
+    } else {
+        b
     }
 }
 
@@ -93,22 +112,8 @@ where
 }
 
 #[cfg(test)]
-mod tests {
+mod add_sub_decimal_tests {
     use super::*;
-
-    #[test]
-    fn test_zero() {
-        assert!(Decimal::<0>::is_zero(&Decimal::<0>::zero()));
-        assert!(Decimal::<1>::is_zero(&Decimal::<1>::zero()));
-        assert!(Decimal::<2>::is_zero(&Decimal::<2>::zero()));
-        assert!(Decimal::<3>::is_zero(&Decimal::<3>::zero()));
-        assert!(Decimal::<4>::is_zero(&Decimal::<4>::zero()));
-        assert!(Decimal::<5>::is_zero(&Decimal::<5>::zero()));
-        assert!(Decimal::<6>::is_zero(&Decimal::<6>::zero()));
-        assert!(Decimal::<7>::is_zero(&Decimal::<7>::zero()));
-        assert!(Decimal::<8>::is_zero(&Decimal::<8>::zero()));
-        assert!(Decimal::<9>::is_zero(&Decimal::<9>::zero()));
-    }
 
     #[test]
     fn test_add_same_prec() {
@@ -146,6 +151,7 @@ mod tests {
     }
 
     #[test]
+    #[allow(clippy::eq_op)]
     fn test_sub_same_prec() {
         let x = Decimal::<3>::new_raw(1234567890);
         let y = x - x;
@@ -178,5 +184,161 @@ mod tests {
     fn test_sub_neg_overflow() {
         let x = Decimal::<4>::new_raw(i128::MIN + 99999);
         let _y = x - Decimal::<4>::TEN;
+    }
+}
+
+impl<T, const P: u8> Add<T> for Decimal<P>
+where
+    T: Integer,
+    i128: std::convert::From<T>,
+    PrecLimitCheck<{ P <= crate::MAX_PREC }>: True,
+{
+    type Output = Self;
+
+    #[inline(always)]
+    fn add(self, other: T) -> Self::Output {
+        Self::Output {
+            coeff: self.coeff + mul_pow_ten(other.into(), P),
+        }
+    }
+}
+
+macro_rules! impl_add_decimal_for_int {
+    () => {
+        impl_add_decimal_for_int!(u8, i8, u16, i16, u32, i32, u64, i64, i128);
+    };
+    ($($t:ty),*) => {
+        $(
+        impl<const P: u8> Add<Decimal<P>> for $t
+        where
+            PrecLimitCheck<{ P <= crate::MAX_PREC }>: True,
+        {
+            type Output = Decimal<P>;
+
+            #[inline(always)]
+            fn add(self, other: Decimal<P>) -> Self::Output {
+                other + self
+            }
+        }
+        )*
+    }
+}
+
+impl_add_decimal_for_int!();
+
+impl<T, const P: u8> Sub<T> for Decimal<P>
+where
+    T: Integer,
+    i128: std::convert::From<T>,
+    PrecLimitCheck<{ P <= crate::MAX_PREC }>: True,
+{
+    type Output = Self;
+
+    #[inline(always)]
+    fn sub(self, other: T) -> Self::Output {
+        Self::Output {
+            coeff: self.coeff - mul_pow_ten(other.into(), P),
+        }
+    }
+}
+
+macro_rules! impl_sub_decimal_for_int {
+    () => {
+        impl_sub_decimal_for_int!(u8, i8, u16, i16, u32, i32, u64, i64, i128);
+    };
+    ($($t:ty),*) => {
+        $(
+        impl<const P: u8> Sub<Decimal<P>> for $t
+        where
+            PrecLimitCheck<{ P <= crate::MAX_PREC }>: True,
+        {
+            type Output = Decimal<P>;
+
+            #[inline(always)]
+            fn sub(self, other: Decimal<P>) -> Self::Output {
+                -other + self
+            }
+        }
+        )*
+    }
+}
+
+impl_sub_decimal_for_int!();
+
+#[cfg(test)]
+mod add_sub_integer_tests {
+    use rust_fixed_point_decimal_core::ten_pow;
+
+    use super::*;
+
+    macro_rules! gen_add_integer_tests {
+        ($func:ident, $t:ty, $p:expr, $coeff:expr) => {
+            #[test]
+            fn $func() {
+                let d = Decimal::<$p>::new_raw($coeff);
+                let i = <$t>::MAX;
+                let r = d + i;
+                assert_eq!(r.precision(), d.precision());
+                assert_eq!(r.coeff, i as i128 * ten_pow($p) + $coeff);
+                let z = i + d;
+                assert_eq!(z.precision(), r.precision());
+                assert_eq!(z.coeff, r.coeff);
+            }
+        };
+    }
+
+    gen_add_integer_tests!(test_add_u8, u8, 2, 1);
+    gen_add_integer_tests!(test_add_i8, i8, 0, 123);
+    gen_add_integer_tests!(test_add_u16, u16, 4, 11);
+    gen_add_integer_tests!(test_add_i16, i16, 4, 1234567);
+    gen_add_integer_tests!(test_add_u32, u32, 1, 0);
+    gen_add_integer_tests!(test_add_i32, i32, 9, 1234);
+    gen_add_integer_tests!(test_add_u64, u64, 3, 321);
+    gen_add_integer_tests!(test_add_i64, i64, 7, 12345678901234567890);
+
+    #[test]
+    fn test_add_i128() {
+        let d = Decimal::<2>::new_raw(1);
+        let i = 12345_i128;
+        let r = d + i;
+        assert_eq!(r.coeff, i * 100 + 1);
+        let z = i + d;
+        assert_eq!(z.precision(), r.precision());
+        assert_eq!(z.coeff, r.coeff);
+    }
+
+    macro_rules! gen_sub_integer_tests {
+        ($func:ident, $t:ty, $p:expr, $coeff:expr) => {
+            #[test]
+            fn $func() {
+                let d = Decimal::<$p>::new_raw($coeff);
+                let i = <$t>::MAX;
+                let r = d - i;
+                assert_eq!(r.precision(), d.precision());
+                assert_eq!(r.coeff, $coeff - i as i128 * ten_pow($p));
+                let z = i - d;
+                assert_eq!(z.precision(), r.precision());
+                assert_eq!(z.coeff, i as i128 * ten_pow($p) - $coeff);
+            }
+        };
+    }
+
+    gen_sub_integer_tests!(test_sub_u8, u8, 2, 1);
+    gen_sub_integer_tests!(test_sub_i8, i8, 0, 123);
+    gen_sub_integer_tests!(test_sub_u16, u16, 4, 11);
+    gen_sub_integer_tests!(test_sub_i16, i16, 4, 1234567);
+    gen_sub_integer_tests!(test_sub_u32, u32, 1, 0);
+    gen_sub_integer_tests!(test_sub_i32, i32, 9, 1234);
+    gen_sub_integer_tests!(test_sub_u64, u64, 3, 321);
+    gen_sub_integer_tests!(test_sub_i64, i64, 7, 12345678901234567890);
+
+    #[test]
+    fn test_sub_i128() {
+        let d = Decimal::<2>::new_raw(501);
+        let i = 12345_i128;
+        let r = d - i;
+        assert_eq!(r.coeff, -i * 100 + 501);
+        let r = i - d;
+        assert_eq!(r.coeff, i * 100 - 501);
     }
 }
