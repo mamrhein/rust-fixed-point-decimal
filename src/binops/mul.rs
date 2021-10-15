391b24ab6ba5ec91c84afe4847e601f1451b4ef6
Michael Amrhein <michael@adrhinum.de>
2021-10-15 17:27:39 +0200

    Replaced macro generating impl of Mul by direct impl.

--- src/binops/mul.rs
+++ src/binops/mul.rs
@@ -68,29 +68,23 @@ mod one_tests {
     }
 }
 
-macro_rules! impl_mul_decimal {
-    (impl $imp:ident, $method:ident) => {
-        impl<const P: u8, const Q: u8> $imp<Decimal<Q>> for Decimal<P>
-        where
-            PrecLimitCheck<{ P <= MAX_PREC }>: True,
-            PrecLimitCheck<{ Q <= MAX_PREC }>: True,
-            PrecLimitCheck<{ (const_sum_u8(P, Q)) <= MAX_PREC }>: True,
-        {
-            type Output = Decimal<{ const_sum_u8(P, Q) }>;
+impl<const P: u8, const Q: u8> Mul<Decimal<Q>> for Decimal<P>
+where
+    PrecLimitCheck<{ P <= MAX_PREC }>: True,
+    PrecLimitCheck<{ Q <= MAX_PREC }>: True,
+    PrecLimitCheck<{ (const_sum_u8(P, Q)) <= MAX_PREC }>: True,
+{
+    type Output = Decimal<{ const_sum_u8(P, Q) }>;
 
-            #[inline(always)]
-            fn mul(self, other: Decimal<Q>) -> Self::Output {
-                Self::Output {
-                    coeff: $imp::$method(self.coeff, other.coeff),
-                }
-            }
+    #[inline(always)]
+    fn mul(self, other: Decimal<Q>) -> Self::Output {
+        Self::Output {
+            coeff: self.coeff * other.coeff,
         }
-
-        forward_ref_binop!(impl $imp, $method);
-    };
+    }
 }
 
-impl_mul_decimal!(impl Mul, mul);
+forward_ref_binop!(impl Mul, mul);
 
 #[cfg(test)]
 mod mul_decimal_tests {
